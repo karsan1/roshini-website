c9a4d118ad217629726383b23a51b38cb75c26d5
Karthick <karthickg.sankar@gmail.com>
2026-09-14 22:50:54 -0400

    change to roshini endpoint

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://karsan1.github.io',
+  site: 'https://roshinisaravanan.github.io',
   base: '/roshini-website',
   output: 'static',
   integrations: [sitemap()],
